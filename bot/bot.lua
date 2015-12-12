49776f059f5296f7cc4a58ce11c1af03cf00eb85
hiddenvirus <hiddenvirus78@gmail.com>
2015-12-12 12:37:41 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {@hidden_virus},
+    sudo_users = {101860317},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
